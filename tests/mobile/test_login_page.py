c2bb10a47561830754d15c5d08ac7e12e3693ab9
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-05-31 14:57:57 +0300

    remow allow coockies, add hide keyboard in mobile tests

--- tests/mobile/test_login_page.py
+++ tests/mobile/test_login_page.py
@@ -18,8 +18,8 @@ def test_login():
         browser.element((AppiumBy.XPATH,
                          '(//android.view.ViewGroup[@resource-id="by.tut.jobs.android:id/item_social_button_container"])[3]')).click()
 
-    with allure.step('Allow coockies'):
-        browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Принять"]')).click()
+    with allure.step('Hide keyboard'):
+        browser.element((AppiumBy.XPATH, '//android.webkit.WebView[@text="Вход в личный кабинет"]/android.view.View/android.view.View/android.view.View[2]')).click()
 
     with allure.step('Press open password button'):
         browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Войти с паролем"]')).click()
